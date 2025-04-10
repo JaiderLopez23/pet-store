7aab93eaf219af082fd68ffe824fd75603e865bb
Jaider Lopez <ljaider920@gmail.com>
2025-04-09 21:53:51 -0500

    supabase database

--- src/config/database.php
+++ src/config/database.php
@@ -1,11 +1,16 @@
 <?php
-    
+     $host        ="aws-0-us-east-2.pooler.supabase.com";
+     $port        ="6543";
+     $dbname      ="postgres";
+     $user        ="postgres.tprexcklrcpreuchonwl";
+     $password    ="unicesmag@@";
+    /*
     $host        ="localhost";
     $port        ="5432";
     $dbname      ="petstore";
     $user        ="postgres";
     $password    ="postgres";
-
+*/
     $data_connection="
         host=$host
         port=$port
